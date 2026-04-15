84d1c131abe9c5444bf87b24328289dc326037ed
blueswayne133 <blueswayne133@gmail.com>
2026-04-15 20:14:32 +0100

    updates

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -38,6 +38,18 @@
             user-select: none;
         }
     </style>
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = 'f8a18700bce470a447a84b2b0a135990667c5d2c';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+</script>
+<noscript>Powered by <a href="https://www.smartsupp.com" target="_blank">Smartsupp</a></noscript>
 
 </head>
 
