a36911d8db110543f5afc127d1af91d2783027c7
blueswayne133 <blueswayne133@gmail.com>
2026-05-03 02:15:32 +0100

    updates

--- resources/views/dashboard/drops.blade.php
+++ resources/views/dashboard/drops.blade.php
@@ -97,7 +97,7 @@
                                         stroke="currentColor" stroke-width="2" stroke-linecap="round"
                                         stroke-linejoin="round" />
                                 </svg>
-                                Unstake &amp; Claim
+                                Click Unstake
                             </button>
                         </form>
                     </div>
